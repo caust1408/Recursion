63061474418bffe390c22aefce9a6b565ec1d471
Craig <cav9871@saintjoe.edu>
2015-01-26 11:35:38 -0500

    Finished the program

--- recursion.js
+++ recursion.js
@@ -1,3 +1,17 @@
 //Craig Austgen
 //Ch.3 recursion problem
 //CMP237
+
+function isEven(n) {
+	 if (n == 0)
+            return true;
+	 else if (n == 1)
+            return false;
+	 else if (n < 0)
+	    return isEven(-n);
+	        else
+	           return isEven(n - 2);
+}
+
+
+
