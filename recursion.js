def08bb585f620671bb5b922b54b7a57f4161b05
Craig <cav9871@saintjoe.edu>
2015-01-26 12:11:08 -0500

    I added my comments to the code

--- recursion.js
+++ recursion.js
@@ -3,14 +3,17 @@
 //CMP237
 
 function isEven(n) {
-	 if (n == 0)
+	 if (n == 0)                    // if n = 0 then it's true
             return true;
-	 else if (n == 1)
+	 else if (n == 1)               // if n = 1 then return false
             return false;
-	 else if (n < 0)
-	    return isEven(-n);
+	 else if (n < 0)                // if n is less then 0 return a negative                                        // of that number
+	    return isEven(-n);       
 	        else
-	           return isEven(n - 2);
+	        return isEven(n - 2);   //subtract n until it reaches 0
+		                        // then if it reaches 0 by subtracting
+					// 2 everytime then it will return true
+					// once it jumps back to the top of the                                         // program
 }
 
 
