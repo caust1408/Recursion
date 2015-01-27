ff32123dea367168bec018fc6197525ec7198073
Craig <cav9871@saintjoe.edu>
2015-01-26 20:53:54 -0500

    I didn't put console.log

--- recursion.js
+++ recursion.js
@@ -3,18 +3,25 @@
 //CMP237
 
 function isEven(n) {
-	 if (n == 0)                    // if n = 0 then it's true
+	 if (n == 0) {                    // if n = 0 then it's true
             return true;
-	 else if (n == 1)               // if n = 1 then return false
+	 }
+	 
+	 else if (n == 1) {               // if n = 1 then return false
             return false;
-	 else if (n < 0)                // if n is less then 0 return a negative                                        // of that number
-	    return isEven(-n);       
-	        else
+	 }
+	 
+	 else if (n < 0) {               // if n is less then 0 return a negative                                        // of that number
+	    return isEven(-n);
+	 }
+	        else  {
 	        return isEven(n - 2);   //subtract n until it reaches 0
-		                        // then if it reaches 0 by subtracting
+		}                 // then if it reaches 0 by subtracting
 					// 2 everytime then it will return true
 					// once it jumps back to the top of the                                         // program
 }
 
-
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
 
